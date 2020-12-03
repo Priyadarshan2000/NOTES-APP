f88e5d9be0d532d2818ded30fe50b7a0554ef4f7
Priyadarshan2000 <priyadarshanghosh26@gmail.com>
2020-12-03 09:52:30 +0530

    Notes-app

---
+++ app/src/main/java/com/EditActivity.kt
@@ -0,0 +1,115 @@
+
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.example.awesomedialog.*
+import com.kikulabs.noteusingroom.dao.NoteDao
+import com.kikulabs.noteusingroom.database.NoteRoomDatabase
+import com.kikulabs.noteusingroom.entity.Note
+import kotlinx.android.synthetic.main.activity_edit.*
+import kotlinx.android.synthetic.main.toolbar_detail.*
+
+class EditActivity : AppCompatActivity(), View.OnClickListener {
+    val EDIT_NOTE_EXTRA = "edit_note_extra"
+    private lateinit var note: Note
+    private var isUpdate = false
+    private lateinit var database: NoteRoomDatabase
+    private lateinit var dao: NoteDao
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit)
+
+        database = NoteRoomDatabase.getDatabase(applicationContext)
+        dao = database.getNoteDao()
+
+        if (intent.getParcelableExtra<Note>(EDIT_NOTE_EXTRA) != null) {
+            button_delete.visibility = View.VISIBLE
+            isUpdate = true
+            note = intent.getParcelableExtra(EDIT_NOTE_EXTRA)!!
+            edit_text_title.setText(note.title)
+            edit_text_body.setText(note.body)
+
+            edit_text_title.setSelection(note.title.length)
+
+        }
+
+        button_save.setOnClickListener(this)
+        button_delete.setOnClickListener(this)
+        nib_back.setOnClickListener(this)
+
+    }
+
+    private fun saveNote(note: Note) {
+
+        if (dao.getById(note.id).isEmpty()) {
+            dao.insert(note)
+        } else {
+            dao.update(note)
+        }
+
+        Toast.makeText(applicationContext, "Note saved", Toast.LENGTH_SHORT).show()
+
+    }
+
+    private fun deleteNote(note: Note) {
+        dao.delete(note)
+        Toast.makeText(applicationContext, "Note removed", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun showDialog() {
+
+        AwesomeDialog.build(this)
+            .position(AwesomeDialog.POSITIONS.CENTER)
+            .body("The note will be permanently deleted.")
+            .icon(R.mipmap.ic_launcher)
+            .onPositive(
+                "Yes, delete",
+                buttonBackgroundColor = android.R.color.white,
+                textColor = ContextCompat.getColor(this, android.R.color.black)
+            ) {
+                deleteNote(note)
+                finish()
+            }
+            .onNegative(
+                "Cancel",
+                buttonBackgroundColor = R.drawable.bg_btn_black,
+                textColor = ContextCompat.getColor(this, android.R.color.white)
+            ) {
+
+            }
+
+    }
+
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.nib_back -> {
+                onBackPressed()
+            }
+            R.id.button_save -> {
+                val title = edit_text_title.text.toString()
+                val body = edit_text_body.text.toString()
+
+                if (title.isEmpty() && body.isEmpty()) {
+                    Toast.makeText(applicationContext, "Note cannot be empty", Toast.LENGTH_SHORT)
+                        .show()
+                } else {
+                    if (isUpdate) {
+                        saveNote(Note(id = note.id, title = title, body = body))
+                    } else {
+                        saveNote(Note(title = title, body = body))
+                    }
+                }
+
+                finish()
+            }
+            R.id.button_delete -> {
+                showDialog()
+            }
+        }
+    }
+}
